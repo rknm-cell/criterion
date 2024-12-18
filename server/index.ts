3b1dee9c979691e50917a31a85a48b4ae067c025
rknm-cell <racknahm@gmail.com>
2024-12-17 21:09:21 -0500

    added api routes for films

--- server/index.ts
+++ server/index.ts
@@ -10,17 +10,87 @@ interface Film {
     picture: string;
 }
 
-let films: Film[] = [];
+let films: Film[] = [
+    {
+        id: 1,
+        title: "The Shawshank Redemption",
+        description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+        picture: "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODU2OTc@._V1_.jpg"
+    }
+
+];
+console.log(films)
 
 function handleAllFilms() {
     return new Response(
+
+
         JSON.stringify(films), {
         headers: { "Content-Type": "application/json" }
     }
     )
 }
 
+function handleFilmsById(id: number) {
+    const film = films.find((film) => film.id === id);
+
+    if (!film) {
+        return new Response("Film not found", { status: 404 });
+    } else {
+        return new Response(
+            JSON.stringify(film), {
+            headers: { "Content-Type": "application/json" }
+        }
+        )
+    }
+};
+
+function handleCreateFilm(title: string, description: string, picture: string) {
+    const newFilm = {
+        id: films.length + 1,
+        title,
+        description,
+        picture,
+    };
+
+    films.push(newFilm);
+
+    return new Response(
+        JSON.stringify(newFilm), {
+        headers: { "Content-Type": "application/json" },
+        status: 201,
+    });
+
+};
+
+function HandleUpdateFilm(id: number, title: string, description: string, picture: string) {
+    const filmIndex = films.findIndex((film) => film.id === id);
+
+    if (filmIndex === -1) {
+        return new Response("Film not found", { status: 404 });
+    }
+
+    films[filmIndex] = {
+        ...film,
+        title,
+        description,
+        picture,
+    };
+    return new Response("Post updated", { status: 200 });
+}
+
+function handleDeleteFilm(id: number) {
+    const filmIndex = films.findIndex((film) => film.id === id);
+
+    if (filmIndex === -1) {
+        return new Response("Film not found", { status: 404 });
+    }
+
+    films.splice(filmIndex, 1);
+    return new Response("Post deleted", { status: 200 });
+}
 serve({
+
     port: PORT,
     async fetch(req) {
         const { method } = req;
@@ -32,14 +102,17 @@ serve({
             const id = match && match?.[1];
 
             if (id) {
-                return new Response('Film', { status: 200 });
+                return handleFilmsById(id);
             }
         }
 
         if (method === 'GET' && pathname === '/api/films') {
+            return handleAllFilms();
 
         }
         if (method === 'POST' && pathname === '/api/films') {
+            const newFilm = await req.json();
+            return handleCreateFilm(newFilm.title, newFilm.description, newFilm.picture);
 
         }
         if (method === 'PATCH' && pathname === '/api/films') {
@@ -47,13 +120,14 @@ serve({
             const id = match && match?.[1];
 
             if (id) {
-                return new Response('Film', { status: 200 });
+                const editedFilm = await req.json();
+
             }
 
         }
 
         return new Response('Not Found', { status: 404 });
     }
-});
+}, console.log("Server started"));
 
 console.log(`Listening on http://localhost:${PORT}`);
