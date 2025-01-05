a4e7fc199e23a96111d19f73caefeef52e031014
rknm-cell <racknahm@gmail.com>
2025-01-05 10:58:28 -0500

    added .macro

--- server/index.ts
+++ server/index.ts
@@ -9,29 +9,7 @@ const db = new PrismaClient()
 const app = new Elysia()
     .use(swagger())
     .use(film)
-    // .post(
-    //     '/register',
-    //     async ({ body }) => db.user.create({
-    //         data: body
-    //     }),
-    //     {
-    //         error({ code }) {
-    //             switch (code) {
-    //                 case 'P2002':
-    //                     return {
-    //                         error: 'Username must be unique'
-    //                     }
-    //             }
-    //         },
-
-    //         body: t.Object({
-    //             email: t.String(),
-    //             password: t.String({
-    //                 minLength: 8
-    //             })
-    //         })
-    //     }
-    // )
+    
     .listen(3000)
 console.log(
     `Elysia is running at ${app.server?.hostname}:${app.server?.port}`
--- server/util/userService.ts
+++ server/util/userService.ts
@@ -1,4 +1,4 @@
-import {Elysia, t} from 'elysia'
+import { Elysia, t } from 'elysia'
 
 export const userService = new Elysia({ name: 'user/service' })
     .state({
@@ -7,8 +7,8 @@ export const userService = new Elysia({ name: 'user/service' })
     })
     .model({
         signIn: t.Object({
-            email: t.String({minLength: 1}),
-            password: t.String({minLength: 8})
+            email: t.String({ minLength: 1 }),
+            password: t.String({ minLength: 8 })
         }),
         session: t.Cookie(
             {
@@ -19,4 +19,26 @@ export const userService = new Elysia({ name: 'user/service' })
             }
         ),
         optionalSession: t.Optional(t.Ref('session'))
+    })
+    .macro({
+        isSignIn(enabled: boolean) {
+            if (!enabled) return
+
+            return {
+                beforeHandle({ error, cookie: { token }, store: { session } }) {
+                    if (!token.value)
+                        return error(401, {
+                            success: false,
+                            message: 'Unauthorized'
+                        })
+                    const username = session[token.value as unknown as number]
+
+                    if (!username)
+                        return error(401, {
+                            success: false,
+                            message: 'Unauthorized'
+                    })
+                }
+            }
+        }
     })
