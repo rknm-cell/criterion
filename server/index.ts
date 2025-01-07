da9f19bb80568d77c40a5daaad51e784b2ee58b2
rknm-cell <racknahm@gmail.com>
2025-01-07 08:50:16 -0500

    removed unneeded code from index

--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,11 @@
-import { Elysia, t } from 'elysia';
+import { Elysia } from 'elysia';
 import { opentelemetry } from '@elysiajs/opentelemetry'
 import { swagger } from '@elysiajs/swagger'
 
 import { film } from './routes/film'
 import { user } from './routes/user'
-import dotenv from 'dotenv';
 
 // Load environment variables from .env file
-dotenv.config();
-console.log(process.env)
-
-console.log('DATABASE_URL:', process.env.DATABASE_URL)
-console.log('JWT_Secret', Bun.env.JWT_SECRET)
 
 const app = new Elysia()
     .use(opentelemetry())
